599702cf44a8ed0fc7a7a4e15b7402e2ff165284
Xue2015 <1161048063@qq.com>
2023-03-06 14:21:42 +0800

    Update ico2base64.py

--- ico2base64.py
+++ ico2base64.py
@@ -3,6 +3,6 @@ import base64
 with open("golive.ico","rb") as i:
     b64str = base64.b64encode(i.read())
     with open("goliveicon.py","ab+") as f:
-        f.write(b64str)
+        f.write("img=b'" +  str(b64str))
 with open("goliveicon.py","a") as f:
     f.write("'")
