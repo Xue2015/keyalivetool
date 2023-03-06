da44d522a52f8d7708fbade7c6ae1908131a8ce3
Xue2015 <1161048063@qq.com>
2023-03-06 14:16:03 +0800

    Create ico2base64.py

---
+++ ico2base64.py
@@ -0,0 +1,8 @@
+import base64
+
+with open("golive.ico","rb") as i:
+    b64str = base64.b64encode(i.read())
+    with open("goliveicon.py","ab+") as f:
+        f.write(b64str)
+with open("goliveicon.py","a") as f:
+    f.write("'")
